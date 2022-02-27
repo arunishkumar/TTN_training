e661736fda9b1eac9ad746873ed0a53c650b8213
Arunish kumar <43927484+arunishkumar@users.noreply.github.com>
2022-02-28 00:02:57 +0530

    Deadlock avoidance using reentract lock

--- Basics_of_Multi_Threading/Q_5/Main5.java
+++ Basics_of_Multi_Threading/Q_5/Main5.java
@@ -1,33 +1,46 @@
-    public class Main5 {  
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main5 { 
+	 private static  String resource1 = "Arunish kumar";  
+     private static String resource2 = "Argentina";  
+    	private static Lock lock = new ReentrantLock();
+    	
+    	private static void lockedResource(int id)
+    	{
+                System.out.println("Thread :"+id+ " locked resource 1 "+resource1);  
+                System.out.println("Thread :"+id+" locked resource 2 "+resource2);  
+           
+    	}
       public static void main(String[] args) {  
-        final String resource1 = "Arunish kumar";  
-        final String resource2 = "Argentina";  
+       
         
                 Thread t1 = new Thread() {  
           public void run() {  
-              synchronized (resource1) {  
-               System.out.println("Thread 1: locked resource 1");  
-      
-               try { Thread.sleep(100);} catch (Exception e) {}  
-      
-               synchronized (resource2) {  
-                System.out.println("Thread 1: locked resource 2");  
-               }  
-             }  
+                lock.lock();
+                try
+                {
+                	lockedResource(1);
+                }
+                finally
+                {
+                	lock.unlock();
+                }
           }  
         };  
       
                 Thread t2 = new Thread() {  
           public void run() {  
-            synchronized (resource2) {  
-              System.out.println("Thread 2: locked resource 2");  
-      
-              try { Thread.sleep(100);} catch (Exception e) {}  
-      
-              synchronized (resource1) {  
-                System.out.println("Thread 2: locked resource 1");  
-              }  
-            }  
+        	  lock.lock();
+              try
+              {
+              	lockedResource(2);
+              }
+              finally
+              {
+              	lock.unlock();
+              }
+           
           }  
         };  
       
