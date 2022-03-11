cb6fb6ce733bb51e1799ad45cb52c6862ffb2d8d
Arunish kumar <43927484+arunishkumar@users.noreply.github.com>
2022-03-11 20:50:04 +0530

    Add files via upload

---
+++ Spring_framework/Q_4/MyCodeApplication.java
@@ -0,0 +1,28 @@
+package com.arunish.project.myCode;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MyCodeApplication {
+
+	public static void main(String[] args) {
+		
+		ConfigurableApplicationContext applicationContext=SpringApplication.run(MyCodeApplication.class, args);
+		
+		BinarySearchImpl obj= applicationContext.getBean(BinarySearchImpl.class);
+		int result=obj.binarySearch(new int[] {3,8,9,7},8);
+		System.out.println(result);
+	
+		//display properties of one bean
+	
+        System.out.println(applicationContext.getBean("bubbleSort").getClass());
+        
+	}
+
+}
